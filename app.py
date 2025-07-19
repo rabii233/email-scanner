493a06f49cde132a123d330871c05ba099b9149f
rabii233 <84365086+rabii233@users.noreply.github.com>
2025-07-19 14:31:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -81,4 +81,4 @@ def scan_emails():
     return jsonify({"emails": unique_results})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
